30a6292c31e122bada2a63bdd213370c3960d3f5
Francesco Farina <rockerg991@gmail.com>
2014-08-09 20:14:40 +0200

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,4 @@
-var splitnlines 	= 	require('./');
+var splitnlines 	= 	require('../');
 var fs 				=	require('fs');
-console.dir(splitnlines);
+
 fs.createReadStream("pg1661.txt").pipe(splitnlines(500)).pipe(process.stdout);
